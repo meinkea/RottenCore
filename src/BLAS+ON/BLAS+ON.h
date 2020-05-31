9daa9d7d3211ace33686ac70162eccb257d5cf6b
Andrew Meinke <meinkea@gmail.com>
2020-05-31 01:52:02 -0400

    Adding the Basic Linear Algebra System header file

---
+++ src/BLAS+ON/BLAS+ON.h
@@ -0,0 +1,23 @@
+/*! /file BLAS+ON.h
+ *  
+ *  Arduino Header file that flattens the includes
+ *
+ *  Arduino doesn't preform searches down beneath the main library directory unless told to0.
+ *  So this header file does just that.
+ *
+ */
+
+
+#ifndef ROTTENCORE__BLAS+ON_H
+  #define ROTTENCORE__BLAS+ON_H
+
+
+  #include "gnd/gnd.h"
+  #include "lvl1/BLAS_lvl1.h"
+  #include "lvl2/BLAS_lvl2.h"
+  #include "lvl3/BLAS_lvl3.h"
+
+
+#endif // ROTTENCORE__BLAS+ON_H
+
+
