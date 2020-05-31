c45686eb885009354c91f8a317a358c1a570e80f
Andrew Meinke <meinkea@gmail.com>
2020-05-31 01:54:09 -0400

    Adding ground header file, backbone of BLAS

---
+++ src/BLAS+ON/gnd/gnd.h
@@ -0,0 +1,19 @@
+/*! /file gnd.h
+ *
+ */
+
+
+#ifndef BLASxOFF_GND_H
+  #define BLASxOFF_GND_H
+
+
+  #include "datatypes/BLAS_datatypes.h"
+  #include "error/BLAS_error.h"
+  #include "memory/BLAS_memory.h"
+  #include "stringf/BLAS_stringf.h"
+  #include "util/BLAS_util.h"
+
+
+#endif // BLASxOFF_GND_H
+
+
