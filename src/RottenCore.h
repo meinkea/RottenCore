c4d300e8abbc7acd15ff02d961790e63f99ee313
Andrew Meinke <meinkea@gmail.com>
2020-05-31 01:20:47 -0400

    Adding main source folder and Library's header file

--- src/RottenCore.h
+++ src/RottenCore.h
@@ -1,15 +1,11 @@
-/*! Arduino Header file that flattens the includes
+/*! /file RottenCore.h 
+ *
+ *  Arduino Header file that flattens the includes
  *
  *  Arduino doesn't preform searches down beneath the main library directory unless told to0.
  *  So this header file does just that.
  *
  */
-
-
-//  /* If we're not using GNU C, elide __attribute__ */
-//  #ifndef __GNUC__
-//  #  define  __attribute__(x)  /*NOTHING*/
-//  #endif
 
 
 #ifndef ROTTENCORE_H
