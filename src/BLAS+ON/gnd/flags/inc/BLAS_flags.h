0ff40659e715b63b2d380c5db39c840d34d1aeb5
Andrew Meinke <meinkea@gmail.com>
2020-05-31 01:56:05 -0400

    Adding BLAS flags

---
+++ src/BLAS+ON/gnd/flags/inc/BLAS_flags.h
@@ -0,0 +1,17 @@
+/*! 
+ *
+ */
+
+
+#ifndef BLASxOFF_BLAS_FLAGS_GENERAL
+  #define BLASxOFF_BLAS_FLAGS_GENERAL
+
+
+  #ifndef ALLOCATED_F
+    #define ALLOCATED_F 1 //!< Set flag for vector allocation
+  #endif // ALLOCATED_F
+
+
+#endif // BLASxOFF_BLAS_FLAGS_GENERAL
+
+
