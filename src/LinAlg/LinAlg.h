3f8cb04a9e93663dafa59605a9e862cb7edd3e57
Andrew Meinke <meinkea@gmail.com>
2020-05-31 02:05:45 -0400

    Adding linear algebra routine's header file

---
+++ src/LinAlg/LinAlg.h
@@ -0,0 +1,21 @@
+/*! /file 
+ * 
+ *  Level one function contain BLAS methods with runtime of O(1)
+ * 
+ */
+
+
+#ifndef ROTTENCORE__LINALG_H
+  #define ROTTENCORE__LINALG_H
+
+
+  #include "inc/s__v_norm1.h"
+  #include "inc/s__v_norm1_v.h"
+  #include "inc/s__v_norm2.h"
+  #include "inc/s__v_norm2_v.h"
+  #include "inc/m__LUdecomp.h"
+
+
+#endif // ROTTENCORE__LINALG_H
+
+
