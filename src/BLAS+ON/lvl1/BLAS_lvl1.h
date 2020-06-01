09dd9edd249cebfe7a5e61f2acfaad6e81fed1e9
Andrew Meinke <meinkea@gmail.com>
2020-05-31 21:09:11 -0400

    Update BLAS_lvl1, minor changes including changes to header guard

--- src/BLAS+ON/lvl1/BLAS_lvl1.h
+++ src/BLAS+ON/lvl1/BLAS_lvl1.h
@@ -5,22 +5,24 @@
  */
 
 
-#ifndef BLASxOFF__LEVEL1_H
-  #define BLASxOFF__LEVEL1_H
+#ifndef BLAS+ON__LEVEL1_H
+  #define BLAS+ON__LEVEL1_H
 
+
+  #include "inc/v__v_add_s.h"
+  #include "inc/v__v_subtract_s.h"
+  #include "inc/v__v_mult_s.h"
+
+  #include "inc/v__v_add_v.h"
+  #include "inc/v__v_subtract_v.h"
+  #include "inc/v__v_mult_v.h"
 
   #include "inc/s__v_dot_v.h"
   #include "inc/s__v_elementsum.h"
-  #include "inc/v__v_add_s.h"
-  #include "inc/v__v_add_v.h"
   #include "inc/v__v_cross3_v.h"
-  #include "inc/v__v_mult_s.h"
-  #include "inc/v__v_mult_v.h"
-  #include "inc/v__v_subtract_s.h"
-  #include "inc/v__v_subtract_v.h"
 
 
 
-#endif // BLASxOFF__LEVEL1_H
+#endif // BLAS+ON__LEVEL1_H
 
 
