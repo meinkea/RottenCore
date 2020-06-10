3a65d9635005a758379c653580f9d9852a5dc951
Andrew Meinke <meinkea@gmail.com>
2020-06-10 17:38:08 -0400

    Fixed Level Z routine includes by adding inc/ to path

--- src/BLASxON/gnd/lvlZ/BLAS_lvlZ.h
+++ src/BLASxON/gnd/lvlZ/BLAS_lvlZ.h
@@ -5,25 +5,25 @@
  */
 
 
-#ifndef BLAS_LEVELZ_H
-  #define BLAS_LEVELZ_H
+#ifndef BLASxON__BLAS_LEVELZ_H
+  #define BLASxON__BLAS_LEVELZ_H
 
 
-  #include "array_add.h"
-  #include "array_ascl.h"
-  #include "array_asum.h"
-  #include "array_copy.h"
-  #include "array_dot.h"
-  #include "array_mscl.h"
-  #include "array_mult.h"
-  #include "array_set.h"
-  #include "array_subtract.h"
-  #include "array_swap.h"
-  #include "array_zero.h"
+  #include "inc/array_add.h"
+  #include "inc/array_ascl.h"
+  #include "inc/array_asum.h"
+  #include "inc/array_copy.h"
+  #include "inc/array_dot.h"
+  #include "inc/array_mscl.h"
+  #include "inc/array_mult.h"
+  #include "inc/array_set.h"
+  #include "inc/array_subtract.h"
+  #include "inc/array_swap.h"
+  #include "inc/array_zero.h"
 
   //#include "check_dim.h"
 
 
-#endif // BLAS_LEVEL0_H
+#endif // BLASxON__BLAS_LEVEL0_H
 
 
