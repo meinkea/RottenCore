7d82c0af810539ed99b3daec60c0ec3c99dfebc0
Andrew Meinke <meinkea@gmail.com>
2020-06-10 17:29:45 -0400

    Made Level Z header file user accessible for testing.

--- src/BLASxON/gnd/gnd.h
+++ src/BLASxON/gnd/gnd.h
@@ -9,6 +9,7 @@
 
   #include "datatypes/BLAS_datatypes.h"
   #include "error/BLAS_error.h"
+  #include "lvlZ/BLAS_lvlZ.h"
   #include "memory/BLAS_memory.h"
   #include "stringf/BLAS_stringf.h"
   #include "util/BLAS_util.h"
