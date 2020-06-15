16afb0caba6fe470f6e8c590b694e7dbe67ad3f9
Andrew Meinke <meinkea@gmail.com>
2020-06-15 17:54:30 -0400

    Replaced routine argument start with the new routine arguments startA and startB.  This allows the loop to start at different indicies in the two arrays, which is necessary for multiplication with matrices.

--- src/BLASxON/gnd/lvlZ/inc/array_dotjump.h
+++ src/BLASxON/gnd/lvlZ/inc/array_dotjump.h
@@ -20,15 +20,16 @@
       __attribute__((always_inline))
       __attribute__((nonull))
     array_dotjump(
-      float * sclRes,            //!< Array pointer where the result will be stored
-      const float * arrOprA,     //!< Array pointer for the 1st operand
-      const float * arrOprB,     //!< Array pointer for the 2nd operand
-      const unsigned int start,  //!< Starting element index to loop across
-      const unsigned int end,    //!< Last element index to loop across
-      const unsigned int jump    //!< Index increment for 2nd operand
+      float * sclRes,             //!< Array pointer where the result will be stored
+      const float * arrOprA,      //!< Array pointer for the 1st operand
+      const float * arrOprB,      //!< Array pointer for the 2nd operand
+      const unsigned int startA,  //!< Starting A element index to loop across
+      const unsigned int startB,  //!< Starting B element index to loop across
+      const unsigned int end,     //!< Last element index to loop across
+      const unsigned int jump     //!< Index increment for 2nd operand
     ) {
       float sum = 0.0f;
-      for(unsigned int I=start, II=start; I<end; ++I, II+=jump) {
+      for(unsigned int I=startA, II=startB; I<end; ++I, II+=jump) {
         sum += arrOprA[I] * arrOprB[II];
       }
       *sclRes = sum;
