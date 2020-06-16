6604971d278a77187ac2c4bc6bdeecb583137b73
Andrew Meinke <meinkea@gmail.com>
2020-06-15 23:55:37 -0400

    Fixed array element calls in loop by adding [I] to arrOprA & arrOprB.

--- src/BLASxON/gnd/lvlZ/inc/array_sub.h
+++ src/BLASxON/gnd/lvlZ/inc/array_sub.h
@@ -22,14 +22,14 @@
     __attribute__((always_inline))
     __attribute__((nonull))
   array_sub(
-    float * arrRes,      //!< Array pointer where the result will be stored
+    float * arrRes,            //!< Array pointer where the result will be stored
     const float * arrOprA,     //!< Array pointer for the 1st operand
     const float * arrOprB,     //!< Array pointer for the 2nd operand
     const unsigned int start,  //!< Starting element index to loop across
     const unsigned int end     //!< Last element index to loop across
   ) {
     for(unsigned int I=start; I<end; ++I) {
-      arrRes[I] = arrOprA - arrOprB;
+      arrRes[I] = arrOprA[I] - arrOprB[I];
     }
     return;
   }
@@ -37,7 +37,7 @@
 
 #ifdef __cplusplus
   }
-#endif 
+#endif
 
 
 #endif // BLASxON__ARRAY_SUBTRACT_H
