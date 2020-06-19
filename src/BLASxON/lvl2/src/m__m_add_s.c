4e1d4aa0ea2e0e3a35975c0c54ed83c4ab2354fa
Andrew Meinke <meinkea@gmail.com>
2020-06-18 20:24:48 -0400

    Added source code for level 2 function m__m_add_s.

--- src/BLASxON/lvl2/src/m__m_add_s.c
+++ src/BLASxON/lvl2/src/m__m_add_s.c
@@ -1,5 +1,28 @@
-/*! /file m__m_add_s
+/*! /file v__v_add_s.c
  *
  */
+
+
+#include "../inc/m__m_add_s.h"
+
+
+// -- Level Z
+#include "../../gnd/lvlZ/inc/array_ascl.h"
+
+
+void m__m_add_s(
+  struct matrix * mRes,
+  const struct matrix * mOpr,
+  const float sclOpr
+) {
+  array_ascl(
+    mRes->m[0],
+    mOpr->m[0],
+    sclOpr,
+    0,
+    mRes->l
+  );
+  return;
+}
 
 
