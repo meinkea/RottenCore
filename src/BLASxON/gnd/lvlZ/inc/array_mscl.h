894b4a6e11802c6ac5a7cd3f07024bdf4457971f
Andrew Meinke <meinkea@gmail.com>
2021-03-02 19:58:13 -0500

    Updates to lvlz functions array_mscl and array_mscladd

--- src/BLASxON/gnd/lvlZ/inc/array_mscl.h
+++ src/BLASxON/gnd/lvlZ/inc/array_mscl.h
@@ -13,31 +13,31 @@
   #include "../../cpu_architecture/cpu_architecture.h"
 
 
-#ifdef __cplusplus
-  extern "C" {
-#endif
-
-
-  static void BLASxON__FuncHEAD
-    __attribute__((always_inline))
-    __attribute__((nonull))
-  array_mscl(
-    float * arrRes,            //!< Array pointer where the result will be stored
-    const float * arrOpr,      //!< Array pointer for the 1st operand
-    const float sclOpr,        //!< float for the 2nd Scalar operand
-    const unsigned int start,  //!< Starting element index to loop across
-    const unsigned int end     //!< Last element index to loop across
-  ) {
-    for(unsigned int I=start; I<end; ++I) {
-      arrRes[I] = sclOpr * arrOpr[I];
+  #ifdef __cplusplus
+    extern "C" {
+  #endif
+  
+  
+    static void BLASxON__FuncHEAD
+      __attribute__((always_inline))
+      __attribute__((nonull))
+    array_mscl(
+      float * arrRes,            //!< Array pointer where the result will be stored
+      const float * arrOpr,      //!< Array pointer for the 1st operand
+      const float sclOpr,        //!< float for the 2nd Scalar operand
+      const unsigned int start,  //!< Starting element index to loop across
+      const unsigned int end     //!< Last element index to loop across
+    ) {
+      for(unsigned int I=start; I<end; ++I) {
+        arrRes[I] = sclOpr * arrOpr[I];
+      }
+      return;
     }
-    return;
-  }
-
-
-#ifdef __cplusplus
-  }
-#endif 
+  
+  
+  #ifdef __cplusplus
+    }
+  #endif 
 
 
 #endif // BLASxON__ARRAY_MSCL_H
--- src/BLASxON/gnd/lvlZ/inc/array_mscladd.h
+++ src/BLASxON/gnd/lvlZ/inc/array_mscladd.h
@@ -15,32 +15,32 @@
   #include "../../cpu_architecture/cpu_architecture.h"
 
 
-#ifdef __cplusplus
-  extern "C" {
-#endif
-
-
-  static void BLASxON__FuncHEAD
-    __attribute__((always_inline))
-    __attribute__((nonull))
-  array_mscladd(
-    float * arrRes,            //!< Array pointer where the result will be stored
-    const float * arrOprA,     //!< Array pointer for the 1st operand
-    const float sclOpr,        //!< float for the 1nd Scalar operand
-    const float * arrOprB,     //!< Array pointer for the 2st operand
-    const unsigned int start,  //!< Starting element index to loop across
-    const unsigned int end     //!< Last element index to loop across
-  ) {
-    for(unsigned int I=start; I<end; ++I) {
-      arrRes[I] = sclOpr*arrOprA[I] + arrOprB[I];
+  #ifdef __cplusplus
+    extern "C" {
+  #endif
+  
+  
+    static void BLASxON__FuncHEAD
+      __attribute__((always_inline))
+      __attribute__((nonull))
+    array_mscladd(
+      float * arrRes,            //!< Array pointer where the result will be stored
+      const float * arrOprA,           //!< Array pointer for the 1st operand
+      const float sclOpr,        //!< float for the 1nd Scalar operand
+      const float * arrOprB,           //!< Array pointer for the 2st operand
+      const unsigned int start,  //!< Starting element index to loop across
+      const unsigned int end     //!< Last element index to loop across
+    ) {
+      for(unsigned int I=start; I<end; ++I) {
+        arrRes[I] = sclOpr*arrOprA[I] + arrOprB[I];
+      }
+      return;
     }
-    return;
-  }
-
-
-#ifdef __cplusplus
-  }
-#endif 
+  
+  
+  #ifdef __cplusplus
+    }
+  #endif 
 
 
 #endif // BLASxON__ARRAY_MSCLADD_H
